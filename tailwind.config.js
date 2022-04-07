9e36e307f55ace73453a3df5efac35100bb08b03
Josh Merlino <31785395+JoshMerlino@users.noreply.github.com>
2022-04-07 19:27:43 -0400

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,11 @@
 module.exports = {
+	mode: "jit",
 	content: [
 		"./app/index.html",
 		"./app/**/*.{vue,js,ts,jsx,tsx}"
 	],
 	theme: {
-		extend: {
-			colors: {
-				body: "#282c34",
-				primary: "#00bcd4"
-			}
-		}
+
 	},
 	plugins: []
 };
